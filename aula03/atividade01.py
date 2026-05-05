87658a6f87b67e4be2afe9c47361ab4221bb102b
CdnCarvalho <claudenirfer@gmail.com>
2026-05-04 21:51:49 -0300

    altera arquivo atividade01

--- aula03/atividade01.py
+++ aula03/atividade01.py
@@ -1,5 +1,5 @@
 valor_compra = float(input('Informe o valor: '))
-if valor > 250:
+if valor_compra > 250:
     desconto = valor_compra * 0.16
     valor_final = valor_compra - desconto
     print(f'Valor com desconto de 16% R$ {valor_final}')
